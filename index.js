7e1feb7fdcc2abf722268da99a26492a3038d2c9
Dibyendu Dhauri <91137992+Dibyendu-Dhauri@users.noreply.github.com>
2023-12-22 17:44:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser())
-app.use(cors({origin:"https://tangerine-pegasus-a09e2c.netlify.app/", credentials:true}))
+// app.use(cors({origin:"https://tangerine-pegasus-a09e2c.netlify.app/", credentials:true}))
+app.use(cors())
 
 
 // mongoDB connection
