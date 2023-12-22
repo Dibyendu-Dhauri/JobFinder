5c354a7fa8116d87429e08b9f311c67f4b867c3c
Dibyendu Dhauri <91137992+Dibyendu-Dhauri@users.noreply.github.com>
2023-12-22 18:30:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser())
-// app.use(cors({origin:"https://tangerine-pegasus-a09e2c.netlify.app/", credentials:true}))
-app.use(cors())
+app.use(cors({origin:"http://localhost:5173", credentials:true}))
+// app.use(cors())
 
 
 // mongoDB connection
@@ -55,5 +55,5 @@ app.use((req, res, next) => {
 
 app.listen( process.env.PORT || 8003, () => {
   connect();
-  console.log("server is starteds");
+  console.log("server is started");
 });
