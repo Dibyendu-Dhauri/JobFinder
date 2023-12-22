b7a4a6341472b7b9eaeb3d8c60476b145fdb5c43
Dibyendu Dhauri <91137992+Dibyendu-Dhauri@users.noreply.github.com>
2023-12-22 19:00:10 +0530

    Update verifyUser.js

--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -4,7 +4,8 @@ const createError = require("../utils/error");
 const verifyUser = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-
+    console.log("inside middleware ",token)
+    
     if (!token) {
       return next(createError(401, "You are not authenticated!"));
     }
