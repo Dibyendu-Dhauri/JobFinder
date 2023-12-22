bb9986ad505636b2950c40c3a8b76fd7ec3a56f6
Dibyendu Dhauri <dhauridibyendu@gmail.com>
2023-12-22 12:33:50 +0530

    update origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser())
-app.use(cors({origin:"*", credentials:true}))
+app.use(cors({origin:"https://jobfinder-api-7rcu.onrender.com", credentials:true}))
 
 
 // mongoDB connection
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
   });
 });
 
-app.listen( process.env.PORT, () => {
+app.listen(process.env.BASE_URL | process.env.PORT, () => {
   connect();
   console.log("server is starteds");
 });
