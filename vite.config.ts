07caf7863b984052ed60982dd23e4fd58b997843
Dibyendu Dhauri <91137992+Dibyendu-Dhauri@users.noreply.github.com>
2023-12-24 21:10:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,16 +7,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig(({mode})=>{
   const env = loadEnv(mode, process.cwd(), '');
+  console.log(env.BASE_URL);
   return {
     plugins: [react()],
     server: {
       proxy: {
-        "/api": {
-          target: env.BASE_URL, // Replace with your backend server
-          changeOrigin: true,
-          secure: false,
-          // rewrite: (path) => path.replace(/^\/api/, ""),
-        },
+         "/api": env.BASE_URL,
+        // "/api": {
+        //   target: env.BASE_URL, // Replace with your backend server
+        //   changeOrigin: true,
+        //   secure: false,
+        // },
       },
     },
   }
