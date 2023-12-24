9681e9bc8ca1339fec8b765852560f4f9218a5ff
Dibyendu Dhauri <91137992+Dibyendu-Dhauri@users.noreply.github.com>
2023-12-24 22:54:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-// import dotenv from 'dotenv'
-// dotenv.config()
+ import dotenv from 'dotenv'
+ dotenv.config()
 
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode})=>{
-  const env = loadEnv(mode, process.cwd(), '');
-  console.log(env.BASE_URL);
+  // const env = loadEnv(mode, process.cwd(), '');
+  // console.log(env.BASE_URL);
   return {
     plugins: [react()],
     server: {
       proxy: {
          // "/api": env.BASE_URL,
         "/api": {
-          target: env.BASE_URL, // Replace with your backend server
+          target: process.env.BASE_URL, // Replace with your backend server
           changeOrigin: true,
           secure: false,
         },
