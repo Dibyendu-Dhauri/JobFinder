3abbd7402b3fe10ba3e30913e93ca6b7d2fad940
Dibyendu Dhauri <91137992+Dibyendu-Dhauri@users.noreply.github.com>
2023-12-24 19:45:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser())
-app.use(cors({origin:"https://tangerine-pegasus-a09e2c.netlify.app", credentials:true}))
+app.use(cors({origin:"https://tangerine-pegasus-a09e2c.netlify.app/", credentials:true}))
 // app.use(cors())
 
 
